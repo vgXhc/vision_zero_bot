b61f322edb1777f04dee91154686990a7512da42
vgXhc <kliems@gmail.com>
2022-07-29 12:48:04 +0200

    fixing breaking change on getting timeline

--- VZ_bot.R
+++ VZ_bot.R
@@ -149,7 +149,7 @@ post_tweet(status = tweet_1,
 
 # prepare disclaimer tweet
 ## lookup status_id
-my_timeline <- get_timeline(rtweet:::home_user())
+my_timeline <- get_my_timeline()
 
 ## ID for reply
 reply_id <- my_timeline$status_id[1]
