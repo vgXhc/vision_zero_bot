fb85549e24fb37bd87fcc9c2093b21543474c900
vgXhc <kliems@gmail.com>
2022-07-29 13:30:20 +0200

    explicitly calling token

--- VZ_bot.R
+++ VZ_bot.R
@@ -149,7 +149,7 @@ post_tweet(status = tweet_1,
 
 # prepare disclaimer tweet
 ## lookup status_id
-my_timeline <- get_my_timeline()
+my_timeline <- get_my_timeline(token = vzbot_token)
 
 ## ID for reply
 reply_id <- my_timeline$status_id[1]
